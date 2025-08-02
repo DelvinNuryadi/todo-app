e0ee7e2709747e0ab3567fb0f7244f638f6cffad
delvinnr <me@delvinuryadi.my.id>
2025-08-02 10:16:33 +0800

    ref: change env format

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 export default function connectDB() {
     mongoose
-        .connect(`${process.env.MONGODB_URI}/todo`)
+        .connect(
+            `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mycluster.bkxmske.mongodb.net/${process.env.DB_NAME}`
+        )
         .then(() => console.log("Database Connected!"))
         .catch((err) => console.log(err));
 }
